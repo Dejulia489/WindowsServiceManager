6af5b27928052a8979cc8c0d6fcfe8c646f1f4de
Anders Thomsen <andth@sydbank.dk>
2019-10-09 10:58:51 +0200

    Add for WinRM: * Session Option parameters * Use SSL

---
+++ WindowsServiceManager/WindowsServiceManagerV4/utility.ps1
@@ -0,0 +1,14 @@
+function Get-NewPSSessionOption {
+    [CmdletBinding()]
+    param(
+        [string] $arguments
+    )
+    Trace-VstsEnteringInvocation $MyInvocation
+    try {
+        $commandString = "New-PSSessionOption $arguments"
+        Write-Verbose "New-PSSessionOption command: $commandString"
+        return (Invoke-Expression -Command $commandString)
+    } finally {
+        Trace-VstsLeavingInvocation $MyInvocation
+    }
+}
